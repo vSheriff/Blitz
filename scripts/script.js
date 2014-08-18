9c47a985e727209bbfe206eb14d2ae43a24aebb0
Muhammad Rizky Wellyanto <wellyanto.rizky@gmail.com>
2014-08-18 09:04:57 +0700

    testing new branch

--- scripts/script.js
+++ scripts/script.js
@@ -10,42 +10,42 @@
         // when the home button is clicked
         if(event.target.id === "buttonHome"){
             // load the content of the home page
-            $("#panelContent").load("../AJAX/homeAJAX.html #panelContent", loaded);
+            $("#panelContent").load("../AJAX/index.html #panelContent", loaded);
 
             // load the panel bottom
-            $("#panelBottom").load("../AJAX/homeAJAX.html #panelBottom", loaded);
+            $("#panelBottom").load("../AJAX/index.html #panelBottom", loaded);
         }
         // when the about button is clicked
         else if (event.target.id === "buttonAbout"){
             // load the content of the about page
-            $("#panelContent").load("../AJAX/aboutAJAX.html #panelContent", loaded);
+            $("#panelContent").load("../AJAX/about.html #panelContent", loaded);
 
             // load the panel bottom
-            $("#panelBottom").load("../AJAX/aboutAJAX.html #panelBottom", loaded);
+            $("#panelBottom").load("../AJAX/about.html #panelBottom", loaded);
         }
         // when the browse button is clicked
         else if (event.target.id === "buttonBrowse") {
             // load the content of the browse page
-            $("#panelContent").load("../AJAX/browseAJAX.html #panelContent", loaded);
+            $("#panelContent").load("../AJAX/browse.html #panelContent", loaded);
 
             // load the panel bottom
-            $("#panelBottom").load("../AJAX/browseAJAX.html #panelBottom", loaded);
+            $("#panelBottom").load("../AJAX/browse.html #panelBottom", loaded);
         }
         // when the account button is clicked
         else if (event.target.id === "buttonAccount") {
             // load the content of the account page
-            $("#panelContent").load("../AJAX/accountAJAX.html #panelContent", loaded);
+            $("#panelContent").load("../AJAX/account.html #panelContent", loaded);
 
             // load the panel bottom
-            $("#panelBottom").load("../AJAX/accountAJAX.html #panelBottom", loaded);
+            $("#panelBottom").load("../AJAX/account.html #panelBottom", loaded);
         }
         // when the account register is clicked
         else if (event.target.id === "buttonRegister") {
             // load the content of the register page
-            $("#panelContent").load("../AJAX/registerAJAX.html #panelContent", loaded);
+            $("#panelContent").load("../AJAX/register.html #panelContent", loaded);
 
             // load the panel bottom
-            $("#panelBottom").load("../AJAX/registerAJAX.html #panelBottom", loaded);
+            $("#panelBottom").load("../AJAX/register.html #panelBottom", loaded);
         }
         // when the social media button is clicked
         else if (event.target.id === "buttonSocialMedia") {
