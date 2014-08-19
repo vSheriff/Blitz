96286d3b706f01c873882b242d8bab2a588c00c8
Muhammad Rizky Wellyanto <wellyanto.rizky@gmail.com>
2014-08-19 11:08:32 +0700

    structure change in the script.js and implementation of the about.html ajax file

--- scripts/script.js
+++ scripts/script.js
@@ -1,51 +1,36 @@
 /**
- * Created by MuhammadRizky on 8/3/14.
+ * Created by Muhammad Rizky on 8/3/14.
  */
 
 $(document).ready(function () {
     'use strict';
 
-    // call back function of getting ajax data
-    function getAjaxData(event) {
+    // when the user clicks something
+    $("input").on('click', function (event) {
         // when the home button is clicked
-        if(event.target.id === "buttonHome"){
+        if (event.target.id === "buttonHome") {
             // load the content of the home page
-            $("#panelContent").load("../AJAX/home.html #panelContent", loaded);
-
-            // load the panel bottom
-            $("#panelBottom").load("../AJAX/home.html #panelBottom", loaded);
+            $("#panelContent").load("../AJAX/home.html #panelContent");
         }
         // when the about button is clicked
-        else if (event.target.id === "buttonAbout"){
+        else if (event.target.id === "buttonAbout") {
             // load the content of the about page
-            $("#panelContent").load("../AJAX/about.html #panelContent", loaded);
-
-            // load the panel bottom
-            $("#panelBottom").load("../AJAX/about.html #panelBottom", loaded);
+            $("#panelContent").load("../AJAX/about.html #panelContent");
         }
         // when the browse button is clicked
         else if (event.target.id === "buttonBrowse") {
             // load the content of the browse page
-            $("#panelContent").load("../AJAX/browse.html #panelContent", loaded);
-
-            // load the panel bottom
-            $("#panelBottom").load("../AJAX/browse.html #panelBottom", loaded);
+            $("#panelContent").load("../AJAX/browse.html #panelContent");
         }
         // when the account button is clicked
         else if (event.target.id === "buttonAccount") {
             // load the content of the account page
-            $("#panelContent").load("../AJAX/account.html #panelContent", loaded);
-
-            // load the panel bottom
-            $("#panelBottom").load("../AJAX/account.html #panelBottom", loaded);
+            $("#panelContent").load("../AJAX/account.html #panelContent");
         }
         // when the account register is clicked
         else if (event.target.id === "buttonRegister") {
             // load the content of the register page
-            $("#panelContent").load("../AJAX/register.html #panelContent", loaded);
-
-            // load the panel bottom
-            $("#panelBottom").load("../AJAX/register.html #panelBottom", loaded);
+            $("#panelContent").load("../AJAX/register.html #panelContent");
         }
         // when the social media button is clicked
         else if (event.target.id === "buttonSocialMedia") {
@@ -53,21 +38,28 @@ $(document).ready(function () {
             // will be further developed in the future
         }
         // when the user clicked the browse text
-        else if (event.target.id === "textSearch"){
+        else if (event.target.id === "textSearch") {
             $(textSearch).val("");
         }
         // when the user clicked the search button
-        else if (event.target.id === "buttonSearch"){
+        else if (event.target.id === "buttonSearch") {
             // move to search the result
             // will be developed much further
         }
-    }
+        // when the user clicks the essay
+        else if (event.target.id === "essayPage") {
+            // load the pdf essay to the content panel
+        }
+    });
 
-    // callback function for future development
-    var loaded = function() {
-        // alert( "Load was performed." );
-    };
-
-    // Register event handlers
-    $("input").on('click', getAjaxData);
+    // when the user presses keyboard keys
+    $("input").keypress(function (event) {
+        if (event.target.id === textSearch) {
+            if (event.which == 13) {
+                if ($("textSearch").val() != "") {
+                    // search the keyword
+                }
+            }
+        }
+    });
 });
