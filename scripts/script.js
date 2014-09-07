93e66dcbc2ac873ede6d3b79ccae3eb74f46f1f4
Muhammad Rizky Wellyanto <wellyanto.rizky@gmail.com>
2014-09-08 00:08:56 +0700

    Script uses MVC structure. Added AngularJS for future development. home ajax file is updated with the sliding essay, although the CSS code doesn't seem to keep up yet.

--- scripts/script.js
+++ scripts/script.js
@@ -5,61 +5,49 @@
 $(document).ready(function () {
     'use strict';
 
-    // when the user clicks something
-    $("input").on('click', function (event) {
-        // when the home button is clicked
-        if (event.target.id === "buttonHome") {
-            // load the content of the home page
-            $("#panelContent").load("../AJAX/home.html #panelContent");
+    /*
+     * MVC structure
+     */
+    var model = {
+        // save method, save state into local storage
+        save: function (event) {
+            // for future development
         }
-        // when the about button is clicked
-        else if (event.target.id === "buttonAbout") {
-            // load the content of the about page
-            $("#panelContent").load("../AJAX/about.html #panelContent");
-        }
-        // when the browse button is clicked
-        else if (event.target.id === "buttonBrowse") {
-            // load the content of the browse page
-            $("#panelContent").load("../AJAX/browse.html #panelContent");
-        }
-        // when the account button is clicked
-        else if (event.target.id === "buttonAccount") {
-            // load the content of the account page
-            $("#panelContent").load("../AJAX/account.html #panelContent");
-        }
-        // when the account register is clicked
-        else if (event.target.id === "buttonRegister") {
-            // load the content of the register page
-            $("#panelContent").load("../AJAX/register.html #panelContent");
-        }
-        // when the social media button is clicked
-        else if (event.target.id === "buttonSocialMedia") {
-            // load the social media
-            // will be further developed in the future
-        }
-        // when the user clicked the browse text
-        else if (event.target.id === "textSearch") {
+    };
+    var view = {
+        // show stuff to the user
+        loadAjaxContent: function (event) {
+            var source = event.target.id.replace("button", "").replace("text", "").toLowerCase();
+            $("#panelContent").load("../AJAX/" + source + ".html #panelContent");
+        },
+        clearTextSearch: function (event) {
             $(textSearch).val("");
         }
-        // when the user clicked the search button
-        else if (event.target.id === "buttonSearch") {
-            // move to search the result
-            // will be developed much further
-        }
-        // when the user clicks the essay
-        else if (event.target.id === "essayPage") {
-            // load the pdf essay to the content panel
-        }
-    });
+    };
+    var controller = {
+        click: function (event) {
+            // clear the text search
+            if (event.target.id === "textSearch") {
+                view.clearTextSearch(event);
+            }
 
-    // when the user presses keyboard keys
-    $("input").keypress(function (event) {
-        if (event.target.id === textSearch) {
-            if (event.which == 13) {
-                if ($("textSearch").val() != "") {
-                    // search the keyword
+            // save any important stuff
+            model.save(event);
+
+            // load the ajax content to the page
+            view.loadAjaxContent(event);
+        },
+        keypress: function (event) {
+            if (event.target.id === textSearch) {
+                if (event.which == 13) {
+                    if ($("textSearch").val() != "") {
+                        // search the keyword
+                    }
                 }
             }
         }
-    });
+    };
+
+    // adding event handler
+    $("input").on('click', controller.click).keypress(controller.keypress);
 });
