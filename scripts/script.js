3aad0130cd6ff1010adef77f59dd91dc24a8acf4
Varlee Sheriff <vSheriff@users.noreply.github.com>
2014-12-18 14:45:07 -0800

    Revert "Registration page added"

--- scripts/script.js
+++ scripts/script.js
@@ -1,53 +1,73 @@
 /**
- * Created by Muhammad Rizky on 8/3/14.
+ * Created by MuhammadRizky on 8/3/14.
  */
 
 $(document).ready(function () {
     'use strict';
 
-    /*
-     * MVC structure
-     */
-    var model = {
-        // save method, save state into local storage
-        save: function (event) {
-            // for future development
-        }
-    };
-    var view = {
-        // show stuff to the user
-        loadAjaxContent: function (event) {
-            var source = event.target.id.replace("button", "").replace("text", "").toLowerCase();
-            $("#panelContent").load("../AJAX/" + source + ".html #panelContent");
-        },
-        clearTextSearch: function (event) {
-            $(textSearch).val("");
-        }
-    };
-    var controller = {
-        click: function (event) {
-            // clear the text search
-            if (event.target.id === "textSearch") {
-                view.clearTextSearch(event);
-            }
+    // call back function of getting ajax data
+    function getAjaxData(event) {
+        // when the home button is clicked
+        if(event.target.id === "buttonHome"){
+            // load the content of the home page
+            $("#panelContent").load("../AJAX/home.html #panelContent", loaded);
 
-            // save any important stuff
-            model.save(event);
-
-            // load the ajax content to the page
-            view.loadAjaxContent(event);
-        },
-        keypress: function (event) {
-            if (event.target.id === textSearch) {
-                if (event.which == 13) {
-                    if ($("textSearch").val() != "") {
-                        // search the keyword
-                    }
-                }
-            }
+            // load the panel bottom
+            $("#panelBottom").load("../AJAX/home.html #panelBottom", loaded);
         }
+        // when the about button is clicked
+        else if (event.target.id === "buttonAbout"){
+            // load the content of the about page
+            $("#panelContent").load("../AJAX/about.html #panelContent", loaded);
+
+            // load the panel bottom
+            $("#panelBottom").load("../AJAX/about.html #panelBottom", loaded);
+        }
+        // when the browse button is clicked
+        else if (event.target.id === "buttonBrowse") {
+            // load the content of the browse page
+            $("#panelContent").load("../AJAX/browse.html #panelContent", loaded);
+
+            // load the panel bottom
+            $("#panelBottom").load("../AJAX/browse.html #panelBottom", loaded);
+        }
+        // when the account button is clicked
+        else if (event.target.id === "buttonAccount") {
+            // load the content of the account page
+            $("#panelContent").load("../AJAX/account.html #panelContent", loaded);
+
+            // load the panel bottom
+            $("#panelBottom").load("../AJAX/account.html #panelBottom", loaded);
+        }
+        // when the account register is clicked
+        else if (event.target.id === "buttonRegister") {
+            // load the content of the register page
+            $("#panelContent").load("../AJAX/register.html #panelContent", loaded);
+
+            // load the panel bottom
+            $("#panelBottom").load("../AJAX/register.html #panelBottom", loaded);
+        }
+        // when the social media button is clicked
+        else if (event.target.id === "buttonSocialMedia") {
+            // load the social media
+            // will be further developed in the future
+        }
+        // when the user clicked the browse text
+        else if (event.target.id === "textSearch"){
+            $(textBrowse).val("");
+        }
+        // when the user clicked the search button
+        else if (event.target.id === "buttonSearch"){
+            // move to search the result
+            // will be developed much further
+        }
+    }
+
+    // callback function for future development
+    var loaded = function() {
+        // alert( "Load was performed." );
     };
 
-    // adding event handler
-    $("input").on('click', controller.click).keypress(controller.keypress);
+    // Register event handlers
+    $("input").on('click', getAjaxData);
 });
